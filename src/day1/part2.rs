2f874a60ee8d6893a8a3189962d7dfc95b071e8f
Marcos Flavio <mfghost69@gmail.com>
2023-06-04 13:09:42 -0300

    Feat (part2): Geeting a result

--- src/day1/part2.rs
+++ src/day1/part2.rs
@@ -1,3 +1,52 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use crate::day1::Elf;
+
 pub fn fd_top_three() -> u32 {
-    42
+    let mut elfs: Vec<Elf>;
+    let     calorie_vec: Vec<Vec<u32>>;
+
+    elfs = Vec::new();
+    if let Ok(vec) = calories_vectorize("inputs/day1.txt") {
+        calorie_vec = vec;
+    } else {
+        panic!("Failed to read from file.");
+    }
+    for cal in calorie_vec {
+        elfs.push(Elf::Calories(cal));
+    }
+    elfs.sort_by(|a, b| b.sum().cmp(&a.sum()));
+    println!("{}", elfs[0].sum()); // Checking to see if the first position is the biggest.
+    elfs[0].sum() + elfs[1].sum() + elfs[3].sum()
+}
+
+fn calories_vectorize(file_path: &str) -> Result<Vec<Vec<u32>>, std::io::Error> {
+    let     file = File::open(file_path)?;
+    let     reader = BufReader::new(file);
+    let mut result: Vec<Vec<u32>>;
+    let mut line_vector: Vec<u32>;
+
+    result = Vec::new();
+    line_vector = Vec::new();
+    for line in reader.lines() {
+        let line = line.expect("Failed to read line");
+        match line.is_empty() {
+            true => {
+                result.push(line_vector);
+                line_vector = Vec::new();
+            },
+            false => {
+                let number:u32 = line
+                    .trim()
+                    .parse()
+                    .expect("failed to parse number");
+                line_vector.push(number);
+            }
+        }
+    }
+
+    if !line_vector.is_empty() {
+        result.push(line_vector);
+    }
+    Ok(result)
 }
