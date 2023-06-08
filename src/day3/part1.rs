f21ef86abcac20a7714a3ab051b4ed11c88ba638
Marcos Flavio <mfghost69@gmail.com>
2023-06-08 02:08:09 -0300

    Feat: Finished day3/part1

--- src/day3/part1.rs
+++ src/day3/part1.rs
@@ -3,12 +3,14 @@ use std::io::{BufRead, BufReader};
 
 pub fn fd_item_type() -> u32 {
     let mut input: Vec<(String, String)>;
+    let priorities: Vec<u32>;
 
     input = Vec::new();
     if let Ok(vec) = get_input("inputs/day3.txt") {
         input = vec;
     }
-    42
+    priorities = get_priorities(input);
+    priorities.iter().sum()
 }
 
 fn get_input(path: &str) -> Result<Vec<(String, String)>, std::io::Error> {
@@ -21,8 +23,31 @@ fn get_input(path: &str) -> Result<Vec<(String, String)>, std::io::Error> {
     result = Vec::new();
     for line in reader.lines() {
         let line = line.expect("Failed to read line");
-        let len = line.len();
-        result.push((String::from(&line[..(len / 2)]), String::from(&line[(len / 2)..])));
+        result.push((String::from(&line[..(line.len() / 2)]), String::from(&line[(line.len() / 2)..])));
     }
     Ok(result)
+}
+
+fn get_priorities(input: Vec<(String, String)>) -> Vec<u32> {
+    let mut result: Vec<u32>;
+    result = Vec::new();
+    for tup in input {
+        let c = get_char(tup.0, tup.1);
+        result.push(get_priority(c));
+    }
+    result
+}
+
+fn get_char(a: String, b: String) -> Option<char> {
+    a.chars().filter(|&c| b.contains(c)).next()
+}
+
+fn get_priority(c: Option<char>) -> u32 {
+    let result: u32;
+    let char: char = c.unwrap();
+    match char.is_uppercase() {
+        true => result = char as u32 - 38,
+        false => result = char as u32 - 96,
+    }
+    result
 }
